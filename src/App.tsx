9e19e92ff13b7c0c0afe85af0667ca84b919ac94
Nick Nisi <nick@nisi.org>
2018-11-19 23:55:50 -0600

    make the Header responsive
    Make the header navigation condense into a hamburger menu on small
    screens with a popout menu on the left side containing the navigation
    links
    
    - rename Menu to Header
    - Make the Header responsive, similar to the original menu on dojo.io

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
 import { tsx } from '@dojo/framework/widget-core/tsx';
 import Outlet from '@dojo/framework/routing/Outlet';
 
-import Menu from './widgets/Menu';
+import Header from './widgets/Header';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Examples from './pages/Examples';
@@ -17,7 +17,7 @@ export default class App extends WidgetBase {
 	protected render() {
 		return (
 			<div classes={[css.root]}>
-				<Menu />
+				<Header />
 				<div classes={[css.content]}>
 					<Outlet key="home" id="home" renderer={() => <Home />} />
 					<Outlet key="blog" id="blog" renderer={() => <Blog />} />
---
+++ src/widgets/Header.tsx
@@ -0,0 +1,80 @@
+import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
+import Resize, { ContentRect } from '@dojo/framework/widget-core/meta/Resize';
+import { tsx } from '@dojo/framework/widget-core/tsx';
+import Link from '@dojo/framework/routing/ActiveLink';
+
+const logo = require('../assets/logo.svg');
+
+import * as css from './Header.m.css';
+
+const pages = ['Blog', 'Documentation', 'Examples', 'Playground', 'Community'];
+
+export default class Menu extends WidgetBase {
+	protected expanded = false;
+
+	protected get isSmall(): boolean {
+		const { isSmall } = this.meta(Resize).get('root', {
+			isSmall: this.smallPredicate
+		});
+
+		if (!isSmall) {
+			this.expanded = false;
+		}
+
+		return isSmall;
+	}
+
+	protected smallPredicate(contentRect: ContentRect) {
+		return contentRect.width < 768;
+	}
+
+	protected toggle() {
+		this.expanded = !this.expanded;
+		this.invalidate();
+	}
+
+	protected close() {
+		this.expanded = false;
+		this.invalidate();
+	}
+
+	protected renderToggleButton(shouldRender: boolean) {
+		if (shouldRender) {
+			return (
+				<a onclick={this.toggle} classes={css.toggleButton} role="button" aria-expanded={`${this.expanded}`}>
+					<span classes={css.srOnly}>Menu</span>
+					<div classes={css.toggleBar}></div>
+				</a>
+			);
+		}
+	}
+
+	protected render() {
+		const { isSmall, expanded } = this;
+		const rootClasses = [css.root];
+		isSmall && rootClasses.push(css.responsive);
+		expanded && rootClasses.push(css.expanded);
+		return (
+			<header key="root" classes={rootClasses}>
+				<div classes={css.left}>
+					<span classes={[css.toggleButtonContainer]}>{this.renderToggleButton(isSmall)}</span>
+					<Link onClick={this.close} to="home" activeClasses={[css.selected]}>
+						<img classes={[css.logo]} alt="logo" src={logo} />
+					</Link>
+				</div>
+				<nav role="navigation" classes={[css.menu]} aria-expanded={!isSmall || expanded} aria-label="Main Menu">
+					<ul>
+						{pages.map((page) => (
+						<li classes={[css.menuItem]}>
+							<Link onClick={this.close} to={page.toLowerCase()} classes={[css.link]} activeClasses={[css.selected]}>
+								{page}
+							</Link>
+						</li>
+						))}
+					</ul>
+				</nav>
+				<div href="" onclick={this.close} classes={[css.backdrop]} tabindex="-1" aria-hidden="true" hidden></div>
+			</header>
+		);
+	}
+}
