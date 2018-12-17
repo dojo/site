38297903e165379a480ad0a2702f9d371830656a
KaneFreeman <lautzd@gmail.com>
2018-12-17 13:09:03 -0500

    Remove intern tests

--- scripts/tests/node.ts
+++
@@ -1,7 +0,0 @@
-import './compile';
-import './logger';
-import './regions/parser';
-import './regions/region-matchers/block-c';
-import './regions/region-matchers/html';
-import './regions/region-matchers/inline-c';
-import './regions/region-matchers/inline-hash';
