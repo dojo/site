2c226c5dc6414fabf3ee77ee10cccf27f7e7e3f7
Nick Nisi <nick@nisi.org>
2019-02-22 14:05:26 -0600

    convert hamburger menu to CSS-only

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,5 +1,4 @@
 import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
-import Resize, { ContentRect } from '@dojo/framework/widget-core/meta/Resize';
 import { tsx } from '@dojo/framework/widget-core/tsx';
 import Link from '@dojo/framework/routing/ActiveLink';
 
@@ -10,61 +9,46 @@ import * as css from './Header.m.css';
 const pages = ['Blog', 'Tutorials', 'Reference Guides', 'Examples', 'Playground', 'Community'];
 
 export default class Menu extends WidgetBase {
-	protected expanded = false;
+	// protected expanded = false;
 
-	private smallPredicate(contentRect: ContentRect): boolean {
-		return contentRect.width < 768;
-	}
+	// private smallPredicate(contentRect: ContentRect): boolean {
+	// 	return contentRect.width < 768;
+	// }
 
-	private toggle() {
-		this.expanded = !this.expanded;
-		this.invalidate();
-	}
+	// private toggle() {
+	// 	this.expanded = !this.expanded;
+	// 	this.invalidate();
+	// }
 
-	private close() {
-		this.expanded = false;
-		this.invalidate();
-	}
+	// private close() {
+	// 	this.expanded = false;
+	// 	this.invalidate();
+	// }
 
-	private onKeyDown(event: KeyboardEvent) {
-		if (event.key === 'Escape') {
-			this.close();
-		}
-	}
+	// private onKeyDown(event: KeyboardEvent) {
+	// 	if (event.key === 'Escape') {
+	// 		this.close();
+	// 	}
+	// }
 
 	protected render() {
-		let { expanded } = this;
-		const { isSmall } = this.meta(Resize).get('root', {
-			isSmall: this.smallPredicate
-		});
-
-		if (!isSmall) {
-			expanded = this.expanded = false;
-		}
-
-		const rootClasses = [css.root];
-		isSmall && rootClasses.push(css.responsive);
-		expanded && rootClasses.push(css.expanded);
 		return (
-			<header key="root" classes={rootClasses} onkeydown={this.onKeyDown}>
+			<header key="root" classes={css.root}>
+				<input id="mainMenuToggle" classes={css.mainMenuToggle} type="checkbox"/>
 				<div classes={[css.left]}>
 					<span classes={css.leftContainer}>
-						{isSmall ? (
-							<button
-								key="toggleButton"
-								onclick={this.toggle}
-								classes={css.toggleButton}
-								aria-expanded={this.expanded}
-							>
-								<span classes={css.srOnly}>Menu</span>
-								<div classes={css.toggleBar} />
-							</button>
-						) : null}
+						<label
+							for="mainMenuToggle"
+							key="toggleButton"
+							classes={css.toggleButton}
+						>
+							<span classes={css.srOnly}>Menu</span>
+							<div classes={css.toggleBar} />
+						</label>
 					</span>
 					<span classes={[css.centerContainer]}>
 						<Link
 							key="homeLink"
-							onClick={this.close}
 							to="home"
 							classes={[css.homeLink]}
 							activeClasses={[css.selected]}
@@ -74,13 +58,12 @@ export default class Menu extends WidgetBase {
 					</span>
 					<span classes={[css.rightContainer]} />
 				</div>
-				<nav role="navigation" classes={[css.menu]} aria-expanded={!isSmall || expanded} aria-label="Main Menu">
+				<nav role="navigation" classes={[css.menu]} aria-label="Main Menu">
 					<ul classes={[css.menuList]}>
 						{pages.map((page) => (
 							<li classes={[css.menuItem]}>
 								<Link
 									key={`${page.toLowerCase().replace(' ', '-')}Link`}
-									onClick={this.close}
 									to={page.toLowerCase().replace(' ', '-')}
 									classes={[css.link]}
 									activeClasses={[css.selected]}
@@ -91,7 +74,6 @@ export default class Menu extends WidgetBase {
 						))}
 					</ul>
 				</nav>
-				<div onclick={this.close} classes={[css.backdrop]} tabindex="-1" aria-hidden="true" hidden />
 			</header>
 		);
 	}
