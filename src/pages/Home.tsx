1587312913dfce43a4e2e099fa08f4c87a9694cb
James Milner <jmilner@sitepen.com>
2019-03-08 16:55:34 +0000

    Add second half of home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,12 +14,12 @@ export default class Home extends WidgetBase {
 		const play = isIntersecting || has('build-time-render');
 
 		return (
-			<div classes={[css.root]}>
-				<div styles={{ backgroundImage: `url(${hero})` }} classes={[css.hero]}>
+			<main classes={[css.root]}>
+				<section styles={{ backgroundImage: `url(${hero})` }} classes={[css.hero]}>
 					<h1 classes={[css.headline]}>A Progressive Framework for Modern Web Apps</h1>
 					<button classes={[css.build]}>Build with Dojo</button>
-				</div>
-				<div classes={[css.ethos]}>
+				</section>
+				<section classes={[css.ethos]}>
 					<Card extraClasses={{ root: css.ethosPoint }}>
 						<div classes={[css.ethosTitleContainer]}>
 							<span classes={[css.productive]}>
@@ -56,8 +56,8 @@ export default class Home extends WidgetBase {
 							applications. Dojo supports inclusivity and provides both.
 						</div>
 					</Card>
-				</div>
-				<div classes={[css.getGoing]}>
+				</section>
+				<section classes={[css.getGoing]}>
 					<h2>Get Going Quickly</h2>
 					<p classes={[css.getGoingHeadline]}>
 						Getting started with Dojo is simple. You can use your command line of choice and{' '}
@@ -105,14 +105,107 @@ export default class Home extends WidgetBase {
 									<span>;</span>
 								</div>
 							</Card>
-							<Card extraClasses={{ root: play ? css.result : css.hide }}>
+							<Card extraClasses={{
+								root: play ? css.result : css.hide,
+								content: css.resultContent 
+							}}>
 								<div classes={[css.check]}>✔</div>
 								<div>Success!</div>
 							</Card>
 						</div>
 					</div>
-				</div>
-			</div>
+				</section>
+				<section>
+					<div classes={[css.featureRow, css.featureOne]}>
+						<h1 classes={[css.featureTitle]}>A Complete Platform</h1>
+						<Card depth={4} extraClasses={{ root: css.featureCardRight }}>
+							<ul classes={[css.featurePoints]}>
+								<li>
+									<h3>Widgets</h3>
+									<div>
+										Write reusable, reactive, components for your page.
+										Dojo allows composition of complex user interfaces
+										via it's widget system. 
+									</div>
+								</li>
+								<li>
+									<h3>Routing</h3>
+									<div>
+										Routing out-of-the-box for your application. Declare 
+										routes and create navigation flows for your web app.
+									</div>
+								</li>
+								<li>
+									<h3>State Management</h3>
+									<div>
+										Managing state can be difficult. With built-in state
+										management via the Dojo Stores system, you can keep
+										track and update your application state efficently.
+									</div>
+								</li>
+							</ul>
+						</Card>
+					</div>
+					<div classes={[css.featureRow, css.featureTwo]}>
+						<Card depth={4} extraClasses={{ root: css.featureCardLeft }}>
+							<ul classes={[css.featurePoints]}>
+								<li>
+									<h3>Centering Modern Standards</h3>
+									<div>
+										Dojo encourages the usage of Browser APIs like 
+										ResizeObserver and IntersectionObserver, rather 
+										than reinventing the wheel.
+									</div>
+								</li>
+								<li>
+									<h3>Dynamic Polyfills</h3>
+									<div>
+										For older browsers Dojo will dynamically load Polyfills
+										for users, just shipping them the things they need.
+									</div>
+								</li>
+								<li>
+									<h3>Compile to Web Components</h3>
+									<div>
+										Allow your Widgets to work across other frameworks
+										by compiling them to native Web Components.
+									</div>
+								</li>
+							</ul>
+						</Card>
+						<h1 classes={[css.featureTitle]}>Leverage the Platform</h1>
+					</div>
+					<div classes={[css.featureRow, css.featureThree]}>
+						<h1 classes={[css.featureTitle]}>Building for Everyone</h1>
+						<Card depth={4} extraClasses={{ root: css.featureCardRight }}>
+							<ul classes={[css.featurePoints]}>
+								<li>
+									<h3>Internationalization</h3>
+									<div>
+										Dojo provides the tools to allow you to 
+										internationalize your application with ease. 
+									</div>
+								</li>
+								<li>
+									<h3>Accessibilility</h3>
+									<div>
+										Dojo Widgets, the widget library provided alongside 
+										the Dojo framework aims to be accessible out-of-the-box.
+									</div>
+								</li>
+								<li>
+									<h3>Lightweight by Default</h3>
+									<div>
+										The core framework for Dojo comes in at 16kb.
+										Dojo code splits on routes, so that code is only fetched, 
+										compiled, and executed when required.
+									</div>
+								</li>
+							</ul>
+						</Card>
+					</div>
+				</section>
+			</main>
 		);
 	}
 }
