4c7b53e2f0d27a672667e3e3b85ee5f76adbf111
James Milner <jmilner@sitepen.com>
2019-03-11 15:08:06 +0000

    Fix 'popping' of whilst scrolling through Get Going section

--- src/pages/home/getgoing/GetGoing.tsx
+++ src/pages/home/getgoing/GetGoing.tsx
@@ -15,8 +15,7 @@ export default class GetGoing extends WidgetBase {
 			<section classes={[css.root]}>
 				<h2>Get Going Quickly</h2>
 				<p classes={[css.headline]}>
-					Getting started with Dojo is simple. You can use your command line of choice and
-					<a href="https://www.npmjs.com">npm</a> to get going quickly.
+					Getting started with Dojo is simple. You can use your command line of choice and <a href="https://www.npmjs.com">npm</a> to get going quickly.
 				</p>
 				<div key="cli" classes={[css.cli]}>
 					<Card dark={true} extraClasses={{ root: css.commands }}>
