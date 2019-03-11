5d4396c78f51a26ac49a730b992f05dea0287560
James Milner <jmilner@sitepen.com>
2019-03-11 13:03:05 +0000

    Break the front page into widgets

---
+++ src/pages/home/ethos/Ethos.tsx
@@ -0,0 +1,51 @@
+import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
+import { tsx } from '@dojo/framework/widget-core/tsx';
+import * as css from './Ethos.m.css';
+import FontAwesomeIcon from '../../../widgets/icon/FontAwesomeIcon';
+import Card from '../../../widgets/card/Card';
+
+export default class Ethos extends WidgetBase {
+	protected render() {
+		return (
+            <section classes={[css.root]}>
+                <Card extraClasses={{ root: css.ethosPoint }}>
+                    <div classes={[css.ethosTitleContainer]}>
+                        <span classes={[css.productive]}>
+                            <FontAwesomeIcon icon="code-branch" />
+                        </span>
+                        <h3 classes={[css.ethosTitle]}>Productive</h3>
+                    </div>
+                    <div>
+                        Dojo enables teams to build web applications with a deliberate approach to productivity,
+                        sustainability and code management.
+                    </div>
+                </Card>
+                <Card extraClasses={{ root: css.ethosPoint }}>
+                    <div classes={[css.ethosTitleContainer]}>
+                        <span classes={[css.adaptable]}>
+                            <FontAwesomeIcon icon="plug" />
+                        </span>
+                        <h3 classes={[css.ethosTitle]}>Adaptable</h3>
+                    </div>
+                    <div>
+                        Intent on not reinventing the wheel, Dojo allows for easy integration with the most powerful
+                        solutions available today on the open web.
+                    </div>
+                </Card>
+                <Card extraClasses={{ root: css.ethosPoint }}>
+                    <div classes={[css.ethosTitleContainer]}>
+                        <span classes={[css.inclusive]}>
+                            <FontAwesomeIcon icon="users" />
+                        </span>
+                        <h3 classes={[css.ethosTitle]}>Inclusive</h3>
+                    </div>
+                    <div>
+                        Demand for accessibility and internationalization are required for enterprise web
+                        applications. Dojo supports inclusivity and provides both.
+                    </div>
+                </Card>
+            </section>
+            
+		);
+	}
+}
---
+++ src/pages/home/hero/Hero.tsx
@@ -0,0 +1,15 @@
+import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
+import { tsx } from '@dojo/framework/widget-core/tsx';
+import * as css from './Hero.m.css';
+const hero = require('../../../assets/herobg.png');
+
+export default class Hero extends WidgetBase {
+	protected render() {
+		return (
+            <section styles={{ backgroundImage: `url(${hero})` }} classes={[css.root]}>
+                <h1 classes={[css.headline]}>A Progressive Framework for Modern Web Apps</h1>
+                <button classes={[css.build]}>Build with Dojo</button>
+            </section>
+		);
+	}
+}
