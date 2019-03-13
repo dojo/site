cc8069e8c4aca3653060dd0d2134d02f25023fc7
Paul Bouchon <mail@bitpshr.net>
2019-03-13 17:34:44 -0400

    feature: add blog index page (#73)

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,14 +1,16 @@
+import Block from '@dojo/framework/widget-core/meta/Block';
 import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
+import i18n from '@dojo/framework/i18n/i18n';
 import { tsx } from '@dojo/framework/widget-core/tsx';
 
-import * as css from './Blog.m.css';
+import Landing from '../widgets/landing/Landing';
+import Post from '../widgets/blog/Post';
+import compiler from '../scripts/compile-blog-index.block';
 
 export default class Blog extends WidgetBase {
 	protected render() {
-		return (
-			<div>
-				<h1 classes={[css.root]}>Blog</h1>
-			</div>
-		);
+		const paths: any = this.meta(Block).run(compiler)({ locale: i18n.locale });
+
+		return <Landing>{paths && paths.map((path: string) => <Post path={path} excerpt />)}</Landing>;
 	}
 }
