db47cb57dfbdf11c32a3c8c6031814ad3504c96d
Daniel Lautzenheiser <lautzd@gmail.com>
2019-05-20 16:14:55 -0400

    WIP

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,19 @@
-#! /bin/sh
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
+	environment="staging"
+else
+	environment="production"
+fi
 
 cp ./now.json ./output/dist
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
-	NOW_URL=${now ./output/dist}
-else
-	NOW_URL=${now ./output/dist}
-fi
+nowurl=$(npx now ./output/dist --token=$NOW_TOKEN --public)
+
+echo "Deployed to $nowurl"
+
+deploymenturl=$(curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST https://api.github.com/repos/dojo/site/deployments -d '{"ref": "'$TRAVIS_COMMIT'","environment": "'$environment'","description": "Deploy request from Travis"}' | jq -r '.url')
+
+echo "Github deployment url $deploymenturl"
+
+echo $(curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST $deploymenturl/statuses -d '{"environment": "'$environment'", "state": "success", "target_url": "'$nowurl'", "log_url": "'$nowurl'/_logs", "environment_url": "'$nowurl'", "description": "Deployment finished successfully."}')
