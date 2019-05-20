113ebca54d9168afe198d0d12ee85bac7f628584
Daniel Lautzenheiser <lautzd@gmail.com>
2019-05-20 16:40:10 -0400

    Update script to exit with error on failure

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,17 @@ fi
 cp ./now.json ./output/dist
 
 nowurl=$(npx now ./output/dist --token=$NOW_TOKEN --public)
+if [ "$nowurl" = "" ] ; then
+	exit 1
+fi
 
 echo "Deployed to $nowurl"
 
 deploymenturl=$(curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST https://api.github.com/repos/dojo/site/deployments -d '{"ref": "'$TRAVIS_COMMIT'","environment": "'$environment'","description": "Deploy request from Travis"}' | jq -r '.url')
+if [ "$deploymenturl" = "" ] ; then
+	exit 1
+fi
 
 echo "Github deployment url $deploymenturl"
 
-echo $(curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST $deploymenturl/statuses -d '{"environment": "'$environment'", "state": "success", "target_url": "'$nowurl'", "log_url": "'$nowurl'/_logs", "environment_url": "'$nowurl'", "description": "Deployment finished successfully."}')
+curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST $deploymenturl/statuses -d '{"environment": "'$environment'", "state": "success", "target_url": "'$nowurl'", "log_url": "'$nowurl'/_logs", "environment_url": "'$nowurl'", "description": "Deployment finished successfully."}'
