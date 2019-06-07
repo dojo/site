b34f00b07af1bbbebe32b35b010ed0f2155c51ea
Daniel Lautzenheiser <lautzd@gmail.com>
2019-06-07 10:09:35 -0400

    Deploy from travis (#110)
    * Enable now deployment from travis

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cp ./now.json ./output/dist
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+	nowurl=$(npx now ./output/dist --token=$PUBLIC_NOW_TOKEN --public)
+	if [ "$nowurl" = "" ] ; then
+		echo "Now deployment failed"
+		exit 1
+	fi
+
+	curl -H "Authorization: token ${PUBLIC_GITHUB_TOKEN}" -H "Content-Type: application/vnd.github.v3+json" -s -X POST "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments" -d '{"body": "This pull request has been deployed to '$nowurl'"}'
+else
+	nowurl=$(npx now ./output/dist --token=$NOW_TOKEN --public --scope=dojo)
+	if [ "$nowurl" = "" ] ; then
+		echo "Now deployment failed"
+		exit 1
+	fi
+
+	echo "Deployed to $nowurl"
+	
+	deploymenturl=$(curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST https://api.github.com/repos/$TRAVIS_REPO_SLUG/deployments -d '{"ref": "'$TRAVIS_COMMIT'","environment": "'$environment'","description": "Deploy request from Travis","auto_merge":false}' | jq -r '.url')
+	if [ "$deploymenturl" = "" ] ; then
+		echo "Failed creating github deployment"
+		exit 1
+	fi
+
+	echo "Github deployment url $deploymenturl"
+
+	curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST $deploymenturl/statuses -d '{"environment": "production", "state": "success", "target_url": "'$nowurl'", "log_url": "'$nowurl'/_logs", "environment_url": "'$nowurl'", "description": "Deployment finished successfully."}'
+fi
