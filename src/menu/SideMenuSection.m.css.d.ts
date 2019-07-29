942d08af6a8ef29d9dc85a37e4146500b675815d
Anthony Gubler <agubler@sitepen.com>
2019-07-29 19:09:02 +0100

    More deletion / tweaks

--- src/menu/SideMenuSection.m.css.d.ts
+++
@@ -1,4 +0,0 @@
-export const root: string;
-export const fixed: string;
-export const title: string;
-export const menuItems: string;
--- src/tutorials/TutorialMenu.nls.ts
+++
@@ -1,9 +0,0 @@
-const messages = {
-	gettingStarted: 'Getting Started',
-	localInstllation: 'Local Installation',
-	sampleTutorial: 'Sample Tutorial',
-	advanced: 'Advanced',
-	anotherTutorial: 'Another Tutorial'
-};
-
-export default { messages };
