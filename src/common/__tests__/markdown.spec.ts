652a4f852ed0d490ce0efdf16d2f6e85fcebbd0d
Anthony Gubler <agubler@sitepen.com>
2019-07-30 18:32:58 +0100

    remove commented code

--- src/common/__tests__/markdown.spec.ts
+++ src/common/__tests__/markdown.spec.ts
@@ -30,42 +30,6 @@ I am another tutorial
 [CodeBlock path=tutorial-2-finished/src/widgets/App.tsx, language=tsx]
 `;
 
-// {
-//     +           "children": Array [
-//     +             Object {
-//     +               "children": Array [
-//     +                 Object {
-//     +                   "children": undefined,
-//     +                   "deferredPropertiesCallback": undefined,
-//     +                   "properties": Object {
-//     +                     "d": "M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z",
-//     +                     "key": "compiled-3",
-//     +                   },
-//     +                   "tag": "path",
-//     +                   "type": "__VNODE_TYPE",
-//     +                 },
-//     +               ],
-//     +               "deferredPropertiesCallback": undefined,
-//     +               "properties": Object {
-//     +                 "class": "refguide-link",
-//     +                 "height": "16",
-//     +                 "key": "compiled-4",
-//     +                 "width": "16",
-//     +               },
-//     +               "tag": "svg",
-//     +               "type": "__VNODE_TYPE",
-//     +             },
-//     +           ],
-//     +           "deferredPropertiesCallback": undefined,
-//     +           "properties": Object {
-//     +             "aria-hidden": "true",
-//     +             "href": "#another-tutorial",
-//     +             "key": "compiled-5",
-//     +           },
-//     +           "tag": "a",
-//     +           "type": "__VNODE_TYPE",
-//     +         }
-
 const mockMarkupOutput = v('section', { key: 'compiled-35' }, [
 	v('h1', { key: 'compiled-6', id: 'another-tutorial' }, [
 		v('a', { key: 'compiled-5', 'aria-hidden': 'true', href: '#another-tutorial' }, [
