c45541cda2a54884bb13c3d22dbc7384587ca3d3
Daniel Lautzenheiser <lautzd@gmail.com>
2020-01-20 14:33:19 -0500

    Fix code sandbox widget test

--- src/code/CodeSandbox.tsx
+++ src/code/CodeSandbox.tsx
@@ -1,6 +1,5 @@
 import { tsx, create } from '@dojo/framework/core/vdom';
 import theme from '@dojo/framework/core/middleware/theme';
-import has, { exists } from '@dojo/framework/core/has';
 
 import * as css from './CodeSandbox.m.css';
 
@@ -12,13 +11,11 @@ const factory = create({ theme }).properties<CodeSandboxProperties>();
 
 export default factory(function CodeSandbox({ middleware: { theme }, properties }) {
 	const themedCss = theme.classes(css);
-
 	const { url } = properties();
-	if (exists('build-time-render') && !has('build-time-render')) {
-		return (
-			<div key="root" classes={[themedCss.root]}>
-				<iframe classes={[themedCss.root]} src={`${url}?autoresize=1&hidenavigation=1`} />
-			</div>
-		);
-	}
+
+	return (
+		<div key="root" classes={[themedCss.root]}>
+			<iframe classes={[themedCss.root]} src={`${url}?autoresize=1&hidenavigation=1`} />
+		</div>
+	);
 });
--- src/code/__tests__/CodeSandbox.spec.tsx
+++ src/code/__tests__/CodeSandbox.spec.tsx
@@ -1,14 +1,9 @@
-import { add } from '@dojo/framework/core/has';
-add('test', true, true);
-
 import assertionTemplate from '@dojo/framework/testing/assertionTemplate';
 import harness from '@dojo/framework/testing/harness';
 import { tsx } from '@dojo/framework/core/vdom';
-import intersection from '@dojo/framework/core/middleware/intersection';
 
 import * as css from '../CodeSandbox.m.css';
 import CodeSandbox from '../CodeSandbox';
-import createIntersectionMock from '@dojo/framework/testing/mocks/middleware/intersection';
 
 const noop: any = () => {};
 
@@ -21,29 +16,17 @@ export const stubEvent = {
 describe('CodeSandbox', () => {
 	const baseAssertion = assertionTemplate(() => (
 		<div key="root" classes={[css.root]}>
-			<iframe assertion-key="iframe" classes={[css.root]} src="" />
+			<iframe
+				assertion-key="iframe"
+				classes={[css.root]}
+				src="https://codesandbox.io/embed/dummy?autoresize=1&hidenavigation=1"
+			/>
 		</div>
 	));
 
 	it('renders', () => {
-		add('build-time-render', false, true);
-		const url = 'https://codesandbox.io/embed/dummy';
+		const h = harness(() => <CodeSandbox url="https://codesandbox.io/embed/dummy" />);
 
-		const intersectionMock = createIntersectionMock();
-
-		const h = harness(() => <CodeSandbox url={url} />, { middleware: [[intersection, intersectionMock]] });
 		h.expect(baseAssertion);
-
-		intersectionMock('root', { isIntersecting: true });
-
-		h.expect(baseAssertion.setProperty('~iframe', 'src', `${url}?autoresize=1&hidenavigation=1`));
-	});
-
-	it('does not render during build-time-rendering', () => {
-		add('build-time-render', true, true);
-		const url = 'https://codesandbox.io/embed/dummy';
-
-		const h = harness(() => <CodeSandbox url={url} />);
-		h.expect(() => undefined);
 	});
 });
